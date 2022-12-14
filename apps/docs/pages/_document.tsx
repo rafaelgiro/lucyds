514f4535af778cbefbe1581e4b977949598625f0
Rafael Giro <me@rafaelgiro.dev>
2022-12-14 19:38:56 -0300

    fix: font loading

--- apps/docs/pages/_document.tsx
+++ apps/docs/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;500;700&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;500;700"
           rel="stylesheet"
         />
       </Head>
